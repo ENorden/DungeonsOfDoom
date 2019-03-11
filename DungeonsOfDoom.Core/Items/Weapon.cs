76c3b52f66adfdd110d1f6b4d1fe9c0855cb30ac
ENorden <elsanord@gmail.com>
2019-03-11 11:43:47 +0100

    Added XML comments to class weapon

--- DungeonsOfDoom.Core/Items/Weapon.cs
+++ DungeonsOfDoom.Core/Items/Weapon.cs
@@ -15,9 +15,12 @@ namespace DungeonsOfDoom.Core.Items
             Power = power;
         }
 
+        /// <summary>
+        /// Generates a weapon type depending on a fixed frequency
+        /// </summary>
+        /// <returns>A weapon type depending on a fixed frequency</returns>
         public static Weapon GenerateWeapon()
         {
-
             if (RandomGen.WithinPercent(15))
                 return new Weapon("Axe", 5);
             else if (RandomGen.WithinPercent(35))
@@ -26,6 +29,10 @@ namespace DungeonsOfDoom.Core.Items
                 return new Weapon("Spear", 2);
         }
 
+        /// <summary>
+        /// Use a weapon from the players' backpack
+        /// </summary>
+        /// <param name="character">The player to use the weapon</param>
         public override void Use(Character character)
         {
             if (character is Player) // Replace this
